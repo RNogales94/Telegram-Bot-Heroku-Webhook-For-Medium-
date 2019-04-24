ea0bffc0ef5f488145b249760ffc985954b3e82a
Rafael Nogales <xrnogales@gmail.com>
2019-04-24 14:29:56 +0200

    Simplify code

--- bot.py
+++ bot.py
@@ -1,48 +1,8 @@
 import requests  
 import os
-from flask import Flask, Response, request
+from flask import Flask, request
 
-BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/' # <--- add your telegram token as environment variable in your system
-
-
-def get_chat_id(data):  
-    """
-    Method to extract chat id from telegram request.
-    """
-    chat_id = data['message']['chat']['id']
-
-    return chat_id
-
-def get_message(data):  
-    """
-    Method to extract message id from telegram request.
-    """
-    message_text = data['message']['text']
-
-    return message_text
-
-def send_message(prepared_data):  
-    """
-    Prepared data should be json which includes at least `chat_id` and `text`
-    """ 
-    message_url = BOT_URL + 'sendMessage'
-    requests.post(message_url, json=prepared_data)  # don't forget to make import requests lib
-
-def change_text_message(text):  
-    """
-    To enable turning our message inside out
-    """
-    return text[::-1]
-
-def prepare_data_for_answer(data):  
-    answer = change_text_message(get_message(data))
-
-    json_data = {
-        "chat_id": get_chat_id(data),
-        "text": answer,
-    }
-
-    return json_data
+BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/'  # <-- add your telegram token as environment variable
 
 
 app = Flask(__name__)
@@ -51,10 +11,18 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def main():  
     data = request.json
-    # I recommend you to see this data in the debugger
 
-    answer_data = prepare_data_for_answer(data)
-    send_message(answer_data)
+    print(data)  # Comment to hide what Telegram is sending you
+    chat_id = data['message']['chat']['id']
+    message = data['message']['text']
+
+    json_data = {
+        "chat_id": chat_id,
+        "text": message,
+    }
+
+    message_url = BOT_URL + 'sendMessage'
+    requests.post(message_url, json=json_data)
 
     return ''
 
